03b0b02e11406317a919bd699d6eb4ec90edc05b
Cocorico101 <83224644+Cocorico101@users.noreply.github.com>
2024-11-07 21:51:34 -0700

    Move the text above the house. Add logo

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
+import './Header.css';
+import Logo from '../assets/images/RYR_logo.svg'; 
 
 function Header() {
   return (
     <Navbar bg="light" expand="lg" className="mb-3">
-      <Navbar.Brand href="/">Rate Your Roommate</Navbar.Brand>
+      <Navbar.Brand href="/">
+        <img src={Logo} alt="Rate Your Roommate" className="navbar-logo" />
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ms-auto d-flex gap-3">
