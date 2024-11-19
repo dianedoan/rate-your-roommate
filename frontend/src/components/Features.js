a626599cc02bc3630975fd4e5705ac9a0efdc316
Diane Doan <diane.doan1@ucalgary.ca>
2024-11-19 05:03:08 -0700

    Added media queries

--- frontend/src/components/Features.js
+++ frontend/src/components/Features.js
@@ -11,17 +11,17 @@ function Features() {
           <Col md={4} className="mb-4">
             <ShieldLock size={40} />
             <h5>Reviews are anonymous</h5>
-            <p>The rating system, each with an optional review, allows users to judge potential roommates based on their past experiences and interactions with other past roommates</p>
+            <p className="feature">The rating system, each with an optional review, allows users to judge potential roommates based on their past experiences and interactions with other past roommates</p>
           </Col>
           <Col md={4} className="mb-4">
             <GeoAlt size={40} />
             <h5>Find roommates in your area</h5>
-            <p>Users can select their location. This information helps narrow down location-based preferences and makes it easier to connect with nearby users. </p>
+            <p className="feature">Users can select their location. This information helps narrow down location-based preferences and makes it easier to connect with nearby users. </p>
           </Col>
           <Col md={4} className="mb-4">
             <ChatDots size={40} />
             <h5>Chat and meet roommates</h5>
-            <p>Users have the ability to message one another so they can get to know each other better and for other inquiries. </p>
+            <p className="feature">Users have the ability to message one another so they can get to know each other better and for other inquiries. </p>
           </Col>
         </Row>
       </Container>
