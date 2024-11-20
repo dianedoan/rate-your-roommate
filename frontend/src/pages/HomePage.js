96aea602e6ab93b5cd63e4321ae4bccfd2451c51
Diane Doan <diane.doan1@ucalgary.ca>
2024-11-20 05:08:26 -0700

    Added more icons; Updated styling

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { Card } from "react-bootstrap";
-import profile1 from "../assets/images/profile-pics/profile1.jpg"; // Import the image
-import profile2 from "../assets/images/profile-pics/profile2.jpg"; // Import the image
-import profile3 from "../assets/images/profile-pics/profile3.jpg"; // Import the image
+import profile1 from "../assets/images/profile-pics/profile1.jpg";
+import profile2 from "../assets/images/profile-pics/profile2.jpg";
+import profile3 from "../assets/images/profile-pics/profile3.jpg";
+import heart2 from '../assets/images/button-icons/heart2.svg'; 
+import heart2clicked from '../assets/images/button-icons/heart2-clicked.svg'; 
+
+
 import "./HomePage.css";
 
 const HomePage = () => {
@@ -18,21 +22,25 @@ const HomePage = () => {
               className="top-rated-profile-image"
             />
           </div>
-          <div className="profile-info">
-            <div className="profile-info">
-              <div className="top-rated-profile-name">Alice Wang</div>
-              <div className="top-rated-profile-occupation">
-                Athlete
-              </div>
-              <div className="top-rated-profile-description">
-                I love skating and sleeping
-              </div>
-              <div className="profile-score"><span className="highlight4">4.5/5</span> Overall Score</div>
+          <div className="top-rated-profile-info">
+            <div className="top-rated-profile-name">Alice Wang</div>
+            <div className="top-rated-profile-occupation">
+              Athlete
             </div>
+            <div className="top-rated-profile-description">
+              I love skating and sleeping
+            </div>
+            <div className="profile-score"><span className="highlight4">4.5/5</span> Overall Score</div>
           </div>
           <div className="location-favorite-container">
-            <p className="location">Calgary, AB</p>
-            <div className="favorite-icon">&#9825;</div>
+            <p className="top-rated-location">Calgary, AB</p>
+            <div className="favorite-icon">
+              <img
+                src={heart2}
+                alt="heart2"
+                className="heart-icon"
+              />
+            </div>
           </div>
         </div>
       </div>
@@ -51,15 +59,23 @@ const HomePage = () => {
             <div className="profile-info">
               <div className="profile-name">Dave Jones</div>
               <div className="profile-score">★★★★☆</div>
-              <div className="profile-description">
+              <div className="profile-occupation">
                 Neurosurgeon
-                <br />I have a passion for biking
+                </div>
+              <div className="profile-description">
+                I have a passion for biking
               </div>
             </div>
           </div>
           <div className="location-favorite-container">
             <p className="location">Airdrie, AB</p>
-            <div className="favorite-icon">&#9825;</div>
+            <div className="favorite-icon">
+              <img
+                src={heart2}
+                alt="heart2"
+                className="heart-icon"
+              />
+            </div>
           </div>
         </div>
 
@@ -75,15 +91,23 @@ const HomePage = () => {
             <div className="profile-info">
               <div className="profile-name">Bob Brown</div>
               <div className="profile-score">★★★★☆</div>
-              <div className="profile-description">
+              <div className="profile-occupation">
                 Student
-                <br />NEED a roommate ASAP
+              </div>
+              <div className="profile-description">
+                NEED a roommate ASAP
               </div>
             </div>
           </div>
           <div className="location-favorite-container">
             <p className="location">Calgary, AB</p>
-            <div className="favorite-icon">&#9825;</div>
+            <div className="favorite-icon">
+              <img
+                src={heart2}
+                alt="heart2"
+                className="heart-icon"
+              />
+            </div>
           </div>
         </div>
           {/* <Card className="explore-card">
