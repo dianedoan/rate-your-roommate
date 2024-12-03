9b579f7be05ab620dfc00f084b97383f0475ebd9
dianedoan <Dianedd419@hotmail.com>
2024-12-02 22:48:03 -0700

    Updated terms and conditions page

--- frontend/src/components/AboutContent.js
+++ frontend/src/components/AboutContent.js
@@ -4,9 +4,12 @@ import '../assets/styles/global.css';
 function AboutContent() {
     return (
         <div className="about-content">
+          <h5>Acceptance of Terms</h5>
+
           <p className="content">
             Have you ever had trouble finding the perfect roommate? Many people often struggle to find roommates who align with their preferences, lifestyle, and living habits. Don't want to waste time vetting too many people and possibly make the wrong decision? Well, look no further, we've got the app for you!
           </p>
+          <h5>Acceptance of Terms</h5>
           <p>
             RATE YOUR ROOMMATE is the perfect web-based application that contains a database of available roommates with their ratings an reviews from verified past roommates readily displayed on their profile. 
           </p>
